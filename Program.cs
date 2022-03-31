8670d1135d2214c9ce5a87c5dbab8a928d28800d
Сергей <seregabur@live.ru>
2022-03-31 21:41:42 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Pasword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = "1234";
+            string secretMessage = "Секретное сообщение";
+            int tryCount = 3;
+
+            for(int i = 0;i<tryCount;i++)
+            {
+                Console.Write("Введите пароль ");
+
+                if(Console.ReadLine()==password)
+                {
+                    Console.WriteLine(secretMessage);
+                    break;
+                }else
+                {
+                    Console.WriteLine("Введен не верный пароль у вас осталось - " + (tryCount - i) + " попыток");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
